73cba94cdda921e95c7cb3eed1cb9e94fd77b1bc
Terry <TerrenceK28@proton.me>
2023-05-21 17:15:28 -0400

    added additional styling and responsiveness to carousel

--- blog-webpage/js/script.js
+++ blog-webpage/js/script.js
@@ -1,7 +1,26 @@
 
+
 new Glide('.glide', {
     type: 'carousel',
     autoplay: 3500, 
     perView: 4,
     gap: 20,
+    breakpoints: {
+        1200: {
+            perView: 3
+        },
+        768: {
+            perView: 2
+        },
+        576: {
+            perView: 1
+        }
+    }
 }).mount();
+
+document.querySelector('.move-up').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+});
